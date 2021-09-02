f5c20d5f45a054cef9052aa96f0a0837161af284
sushant2931 <sushantlad8888@gmail.com>
2021-09-03 01:25:52 +0530

    Empty Board Is Created

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,26 @@
 package bl.com.tictactoe;
 
 public class TicTacToeGame {
+	private static char[] element;
+
 	public static void main(String[] args) {
 
 		// Displaying TicTacTae Game Welcome Message
 		System.out.println("*------Welcome to Tic Tac Toe Game------*");
+		// for creating empty elements
+		boardCreation();
+	}
+
+	/*
+	 * creating elements with null 0th index ignored for user friendly
+	 * 
+	 * @param element
+	 */
+	private static void boardCreation() {
+		element = new char[10];
+		for (int i = 1; i < 10; i++) {
+			element[i] = ' ';
+		}
+		System.out.println("Empty board created");
 	}
 }
